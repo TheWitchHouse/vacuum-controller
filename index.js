712cf84d49e4f0d5ee5d96c734c8f9caaf081e10
r4dx <r4dx@bk.ru>
2019-09-15 21:03:56 +0300

    Passing consul and listen port thru command line arguments for easier docker support

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
-const settings = require('./settings')
+'use strict'
 
-const consul = require('consul')( { host: settings.consul_host, promisify: true } )
+const consul_module = require('consul')
 const miio = require('miio')
 const express = require('express')
 
-const getVacuum = async function() {
-  console.info("Loading configuration...")
-  const host = (await consul.kv.get('the-witch-house/config/vacuum/host')).Value
-  const token = (await consul.kv.get('the-witch-house/config/vacuum/token')).Value
 
-  console.info("Connecting to vacuum " + host + "; token is " + token)
-  const vacuum = await miio.device({ address: host, token: token })
-  console.info("Connected to ", vacuum)
-  return vacuum
-}
+if (process.argv.length != 4)
+  throw "Usage: node index.js [CONSUL_HOST] [LISTEN_PORT]. \nYou've passed: " + process.argv
 
-const withVacuum = async function(job) {
-  var vacuum = await getVacuum()
-  try {
-    await job(vacuum)
-  }
-  finally {
-    console.info("Disconnecting...")
-    vacuum.destroy()
-    console.info("Disconnected...")
-  }
-}
+const listen_port = Number(process.argv[3])
+
+if (!Number.isInteger(listen_port) || listen_port <= 0 || listen_port > 65535)
+  throw "Incorrect port - expected integer between 1 and 65535, you've passed: " + process.argv[3] + ' which converts to ' + listen_port
+
+
+const consul_host = process.argv[2]
+
+const consul = consul_module( { host: consul_host, promisify: true } )
 
 const server = express()
+server.listen(listen_port, function () {
+  console.info('Listening on port ' + listen_port)
+})
+
+
 server.get('/start', async function (request, response) {
   console.info('Starting the vacuum...')
   await withVacuum(async function(vacuum) {
@@ -46,7 +42,27 @@ server.get('/stop', async function (request, response) {
   })
   response.send('OK')
 })
-      
-server.listen(settings.listening_port, function () {
-  console.info('Listening on port ' + settings.listening_port)
-})
+
+
+const withVacuum = async function(job) {
+  var vacuum = await getVacuum()
+  try {
+    await job(vacuum)
+  }
+  finally {
+    console.info("Disconnecting...")
+    vacuum.destroy()
+    console.info("Disconnected...")
+  }
+}
+
+const getVacuum = async function() {
+  console.info("Loading configuration...")
+  const host = (await consul.kv.get('the-witch-house/config/vacuum/host')).Value
+  const token = (await consul.kv.get('the-witch-house/config/vacuum/token')).Value
+
+  console.info("Connecting to vacuum " + host + "; token is " + token)
+  const vacuum = await miio.device({ address: host, token: token })
+  console.info("Connected to ", vacuum)
+  return vacuum
+}
--- settings.js
+++
@@ -1,2 +0,0 @@
-module.exports.consul_host = 'cybernetic'
-module.exports.listening_port = 3000
